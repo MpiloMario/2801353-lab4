d1c3c0caef05e1a9a10c75137b1e9cc43fadd49d
MpiloMario <vmuser@twk108-03-06.ms.wits.ac.za>
2026-03-02 14:30:56 +0200

    Initial features

---
+++ script.js
@@ -0,0 +1,21 @@
+async function searchCountry(countryName) {
+    try{
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+    const data = await response.json;
+    document.getElementById('country-info').innerHTML = `
+    <h2>${country.name.common}</h2>
+    <p><strong>Capital:</strong> ${country.capital[0]}</p>
+    <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+    <p><strong>Region:</strong> ${country.region}</p>
+    <img src="${country.flags.svg}" alt="${country.name.common} flag">
+`;
+    } catch(error){
+
+    } finally{
+
+    }
+}
+document.getElementById('search-btn').addEventListener('click', () => {
+    const country = document.getElementById('country-input').value;
+    searchCountry(country);
+});
